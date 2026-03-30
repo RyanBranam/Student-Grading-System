b7bac40f19b84851e1e8ed1baca89ffdebc0e5a7
RyanBranam <branamryan@gmail.com>
2026-03-30 17:20:03 -0400

    Implemented Grade constructor

--- src/Grade.cpp
+++ src/Grade.cpp
@@ -1,1 +1,5 @@
+#include "Grade.h"
 
+// Initializes Grade object with category, score, maxScore, and weight
+Grade::Grade(std::string category, double score, double maxScore, doouble weight)
+    : category(category), score(score), maxScore(maxScore), weight(weight) {}
