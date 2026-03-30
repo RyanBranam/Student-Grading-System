27078286bf445480695b6b27947fb42a215c008a
RyanBranam <branamryan@gmail.com>
2026-03-30 17:29:02 -0400

    Added basic getter functions to Grade class

--- src/Grade.cpp
+++ src/Grade.cpp
@@ -3,3 +3,13 @@
 // Initializes Grade object with category, score, maxScore, and weight
 Grade::Grade(std::string category, double score, double maxScore, doouble weight)
     : category(category), score(score), maxScore(maxScore), weight(weight) {}
+
+// Returns score earned for that grade
+    double Grade::getScore() const {
+        return score;
+    }
+
+// Return category of the grade (exam, hw, quiz)
+    std::string Grade::getCategory() const {
+        return category;
+    }
