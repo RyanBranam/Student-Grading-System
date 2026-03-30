67367e2c27e76023db783822b7a4b9ead5d3a63e
RyanBranam <branamryan@gmail.com>
2026-03-30 16:56:03 -0400

    Added method declarations to Grade class

--- include/Grade.h
+++ include/Grade.h
@@ -11,7 +11,14 @@ private:
     double weight;
 
 public:
-    Grade();
+    Grade(std::string category, double score, double maxScore, double weight);
 
+    double getScore() const;
+    double getPercentage() const;
+    std::string getCategory() const;
 
-}
+void setScore(double score);
+
+};
+
+#endif
