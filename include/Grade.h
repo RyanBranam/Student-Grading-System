e28c57a5544acabf7aa80171bc83970da0b145ad
RyanBranam <branamryan@gmail.com>
2026-03-30 16:44:00 -0400

    Created Grade class structure in header file

--- include/Grade.h
+++ include/Grade.h
@@ -1,1 +1,17 @@
+#ifndef Grade_h
+#define Grade_h
 
+#include <string>
+
+class Grade {
+private:
+    std::string category;
+    double score;
+    double maxScore;
+    double weight;
+
+public:
+    Grade();
+
+
+}
