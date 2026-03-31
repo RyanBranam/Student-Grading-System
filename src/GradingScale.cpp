63310d940dd3938f79877d5fe784d950c654e32c
Neison2x <neisonsolares2006@gmail.com>
2026-03-31 15:28:57 -0400

    Fix comments formatting in GradingScale.cpp

--- src/GradingScale.cpp
+++ src/GradingScale.cpp
@@ -1,10 +1,10 @@
 #include "GradingScale.h"
 
-// Default constructor sets standard 90/80/70/60 scale
+// default constructor sets standard 90/80/70/60 scale
 GradingScale::GradingScale()
     : aMin(90.0), bMin(80.0), cMin(70.0), dMin(60.0) {}
 
-// Updates the grade boundaries with custom values
+// updates the grade boundaries with custom values
 void GradingScale::setScale(double a, double b, double c, double d) {
     aMin = a;
     bMin = b;
@@ -12,7 +12,7 @@ void GradingScale::setScale(double a, double b, double c, double d) {
     dMin = d;
 }
 
-// Returns letter grade based on percentage
+// returns letter grade based on percentage
 std::string GradingScale::assignLetterGrade(double percentage) const {
     if (percentage >= aMin) return "A";
     if (percentage >= bMin) return "B";
