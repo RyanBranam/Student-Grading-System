ede023a6c3cd29a059b91793bda05c3fdfb5ee7f
Neison2x <neisonsolares2006@gmail.com>
2026-03-31 15:34:43 -0400

    Update InputValidator.h

--- include/InputValidator.h
+++ include/InputValidator.h
@@ -1,1 +1,18 @@
+#ifndef InputValidator_h
+#define InputValidator_h
 
+#include <string>
+
+class InputValidator {
+public:
+    // returns true if score is between 0 and maxScore
+    static bool isValidGrade(double score, double maxScore);
+
+    // returns true if studentId is a positive integer
+    static bool isValidStudent(int studentId);
+
+    // goes to true if the string is not empty or only whitespace
+    static bool isNonEmptyString(const std::string& str);
+};
+
+#endif
