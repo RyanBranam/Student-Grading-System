ce96888c9a253051a8ca766fcadad2f68b7fa97a
Neison2x <neisonsolares2006@gmail.com>
2026-03-31 15:25:42 -0400

    Implement GradingScale class with grade boundaries

--- src/GradingScale.cpp
+++ src/GradingScale.cpp
@@ -1,1 +1,22 @@
+#include "GradingScale.h"
 
+// Default constructor sets standard 90/80/70/60 scale
+GradingScale::GradingScale()
+    : aMin(90.0), bMin(80.0), cMin(70.0), dMin(60.0) {}
+
+// Updates the grade boundaries with custom values
+void GradingScale::setScale(double a, double b, double c, double d) {
+    aMin = a;
+    bMin = b;
+    cMin = c;
+    dMin = d;
+}
+
+// Returns letter grade based on percentage
+std::string GradingScale::assignLetterGrade(double percentage) const {
+    if (percentage >= aMin) return "A";
+    if (percentage >= bMin) return "B";
+    if (percentage >= cMin) return "C";
+    if (percentage >= dMin) return "D";
+    return "F";
+}
