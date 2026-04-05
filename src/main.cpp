1e47c315ac72854d5598a34b5f2af59a9c913c65
Jessica-V-Costeno <jvasquezcosteno9942@eagle.fgcu.edu>
2026-04-04 20:06:32 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include "Grade.h"
 #include "Student.h"
 #include "GradingScale.h"
+#include "GradingSystem.h"
 
 
 int main() {
@@ -39,16 +40,107 @@ int main() {
         scale.setScale(85, 75, 65, 55);
         std::cout << "Custom scale, 84 -> " << scale.assignLetterGrade(84) << std::endl;
 */
-
-
+    GradingSystem system;
     int choice;
-
-    std::cout << "Welcome to the Student Grading System!\n";
-    std::cout << "1. Add Student\n";
-
-    std::cin>>choice;
-
-
-
+    do {
+        std::cout << "Welcome to the Student Grading System!\n";
+        std::cout << "1. Add Student\n";
+        std::cout << "2. Edit Student\n";
+        std::cout << "3. Delete Student\n";
+        std::cout << "4. Enter Grades for Student\n";
+        std::cout << "5. Generate Report Cards\n";
+        std::cout << "6. Search for Student\n";
+        std::cout << "7. Exit\n";
+        std::cout << "Choose an option: ";
+        
+        std::cin >> choice;
+        switch (choice) {
+            case 1: {
+                int tempId;
+                std::string tempName;
+                std::cout << "Enter Student ID of student you would like to add: ";
+                std::cin >> tempId;
+                std::cin.ignore();
+                std::cout << "Enter Name of student you would like to add: ";
+                std::getline(std::cin, tempName);
+                system.addStudent(tempId, tempName);
+                break; }
+            case 2: {
+                int tempId;
+                std::string newName, newBehavior, newComments;
+                std::cout << "Enter Student ID of student you would like to edit: ";
+                std::cin >> tempId;
+                std::cin.ignore();
+                Student* s = system.searchStudent(tempId);
+                if (s == nullptr) {
+                    std::cout << "Student with ID " << tempId << " not found.\n";
+                    break; }
+                else {
+                    std::cout << "Student found\n";
+                    std::cout << "Enter new name for student or press enter to keep current name: ";
+                    std::getline(std::cin, newName);
+                    if (!newName.empty()) s->setName(newName);
+                    
+                    std::cout << "Enter new behavior notes for student or press enter to keep current behavior: ";
+                    std::getline(std::cin, newBehavior);
+                    if (!newBehavior.empty()) s->setBehavior(newBehavior);
+                    
+                    std::cout << "Enter new comments notes for student or press enter to keep current comments: ";
+                    std::getline(std::cin, newComments);
+                    if (!newComments.empty()) s->setComments(newComments); }
+                break; }
+            case 3: {
+                int tempId;
+                std::cout << "Enter Student ID of student you would like to delete: ";
+                std::cin >> tempId;
+                std::cin.ignore();
+                system.deleteStudent(tempId);
+                break; }
+            case 4: {
+                int tempId;
+                std::cout << "Enter Student ID of student you would like to enter grades for: ";
+                std::cin >> tempId;
+                std::cin.ignore();
+                Student* s = system.searchStudent(tempId);
+                if (s == nullptr) {
+                    std::cout << "Student with ID " << tempId << " not found.\n";
+                    break; }
+                else {
+                    std::cout << "Student found\n";
+                    std::string category;
+                    double score, maxScore, weight;
+                    std::cout << "Enter a grade category (Ex: Exam, Homework, etc.): ";
+                    std::getline(std::cin, category);
+                    std::cout << "Enter score earned for this grade: ";
+                    std::cin >> score;
+                    std::cout << "Enter max possible score for this grade: ";
+                    std::cin >> maxScore;
+                    std::cout << "Enter weight of this grade toward final grade (Ex: 0.0-1.0): ";
+                    std::cin >> weight;
+                    s->addGrade(category, score, maxScore, weight); }
+                break; }
+            case 5: {
+                system.generateReports();
+                break; }
+            case 6: {
+                int tempId;
+                std::cout << "Enter Student ID of student you would like to search for: ";
+                std::cin >> tempId;
+                std::cin.ignore();
+                Student* s = system.searchStudent(tempId);
+                if (s == nullptr) {
+                    std::cout << "Student with ID " << tempId << " not found.\n";
+                    break; }
+                else {
+                    std::cout << "Student found: " << s->getName() << "\n"; }
+                break; }
+            case 7:
+                std::cout << "Goodbye!\n";
+                break;
+            default: {
+                std::cout << "Sorry that is an invalid option.\n";
+                break; } }
+    } while (choice != 7);
+    
     return 0;
 }
