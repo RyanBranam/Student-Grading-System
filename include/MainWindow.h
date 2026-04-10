f98f317ca098685376533856a0c67782678acbb2
RyanBranam <branamryan@gmail.com>
2026-04-10 15:19:19 -0400

    Added student table and GUI controls

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -1,10 +1,26 @@
 #pragma once
 #include <QMainWindow>
 
+QT_BEGIN_NAMESPACE
+class QTableWidget;
+class QPushButton;
+class QTextEdit;
+QT_END_NAMESPACE
+
 class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget* parent = nullptr);
     ~MainWindow();
+
+private:
+    void setupUi();
+
+    QTableWidget* studentTable;
+    QPushButton* addStudentButton;
+    QPushButton* addGradeButton;
+    QPushButton* generateReportButton;
+    QPushButton* refreshButton;
+    QTextEdit* outputBox;
 };
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,8 +1,58 @@
 #include "MainWindow.h"
 
+#include <QWidget>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QTableWidget>
+#include <QHeaderView>
+#include <QPushButton>
+#include <QTextEdit>
+#include <QAbstractItemView>
+
 MainWindow::MainWindow(QWidget* parent)
-    : QMainWindow(parent)
-{
-    setWindowTitle("Student Grading System");
+    : QMainWindow(parent),
+    studentTable(nullptr),
+    addStudentButton(nullptr),
+    addGradeButton(nullptr),
+    generateReportButton(nullptr),
+    refreshButton(nullptr),
+    outputBox(nullptr) {
+    setupUi();
 }
-MainWindow::~MainWindow() = default;
+    MainWindow::~MainWindow() = default;
+
+void MainWindow::setupUi() {
+    setWindowTitle("Student Grading System");
+    resize(950, 600);
+
+    QWidget* centralWidget = new QWidget(this);
+    setCentralWidget(centralWidget);
+
+    QVBoxLayout* mainLayout = new QVBoxLayout(centralWidget);
+    QHBoxLayout* buttonLayout = new QHBoxLayout();
+
+    studentTable = new QTableWidget(this);
+    studentTable->setColumnCount(4);
+    studentTable->setHorizontalHeaderLabels({"ID", "Name", "Average", "Letter Grade"});
+    studentTable->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    studentTable->setSelectionBehavior(QAbstractItemView::SelectRows);
+    studentTable->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
+    addStudentButton = new QPushButton("Add Student", this);
+    addGradeButton = new QPushButton("Add Grade", this);
+    generateReportButton = new QPushButton("Generate Report", this);
+    refreshButton = new QPushButton("Refresh", this);
+
+    outputBox = new QTextEdit(this);
+    outputBox->setReadOnly(true);
+    outputBox->setPlaceholderText("Messages and reports will appear here...");
+
+    buttonLayout->addWidget(addStudentButton);
+    buttonLayout->addWidget(addGradeButton);
+    buttonLayout->addWidget(generateReportButton);
+    buttonLayout->addWidget(refreshButton);
+
+    mainLayout->addLayout(buttonLayout);
+    mainLayout->addWidget(studentTable);
+    mainLayout->addWidget(outputBox);
+}
